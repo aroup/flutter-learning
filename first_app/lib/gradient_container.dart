dc5639d7d086791d43d944234901d2134173b7d1
Aroup Goldar Dhruba <aroupdhruba@gmail.com>
2024-04-28 00:40:52 +0600

    Complete first basic app

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,22 +1,31 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.colors});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  final List<Color> colors;
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: colors, begin: startAlignment, end: endAlignment),
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
       ),
       child: const Center(
-        child: StyledText('Hello world!'),
+        child: DiceRoller(),
       ),
     );
   }
